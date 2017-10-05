cbfda9ab49aca8fbb3679e290747d8efaae93cbe
Rakiiii <sudaru2@gmail.com>
2017-10-05 22:22:43 +0300

    refactored

--- main.c
+++ main.c
@@ -3,63 +3,64 @@
 
 
 
-int Game(int Number);
-int SetAmountOfTries();
-int StartGame();
+int Game( int Number ) ;
+int SetAmountOfTries() ;
+int StartGame() ;
 
-int Game(int Number) {
-    int Idea = 0;
-    printf("Print your ideas_ ");
-    Idea = In();
-    if(Idea == Number)
+int Game( int Number ) //сама игра
+{
+    int Idea = 0 ; //предположение пользователя
+    printf( "Print your ideas_ " ) ;
+    Idea = In() ; //получаем предположение
+    if( Idea == Number ) //если совподает то выходим из обеих функций
     {
-        printf("You are win! My congratulations to you!\n");
-        printf("Press any button to restart the game\n");
-        getch();
-        return -1;
-    }else if (Idea < Number)
+        printf( "You are win! My congratulations to you!\n" ) ;
+        printf( "Press any button to restart the game\n" ) ;
+        getch() ;
+        return -1 ;
+    }else if ( Idea < Number )//если не угадали возвращаемся в цикл
     {
-        printf("Try bigger one\n");
-        return 0;
+        printf( "Try bigger one\n" ) ;
+        return 0 ;
     }
     else
     {
-        printf("Try smaller one\n");
+        printf( "Try smaller one\n" ) ;
         return 0;
     }
 }
 
 
-int SetAmountOfTries()
+int SetAmountOfTries() //получаем количество попыток в подходящем количестве
 {
-    system("cls");
-    printf("Choose Amount of tries_\n");
-    int Amount = In();
-    while(Amount < 1 || Amount > 50)
+    system( "cls" ) ;
+    printf( "Choose Amount of tries_\n" ) ;
+    int Amount = In() ;
+    while( Amount < 1 || Amount > 50 )
     {
-        if(Amount > 50)printf("It's too easy, set less amount of tries_ \n");
-        else printf("It's impossible, set more tries_ \n");
-        Amount = In();
+        if( Amount > 50 ) printf( "It's too easy, set less amount of tries_ \n" ) ;
+        else printf( "It's impossible, set more tries_ \n" ) ;
+        Amount = In() ;
     }
-    system("cls");
-    return Amount;
+    system( "cls" ) ;
+    return Amount ;
 }
 
-int StartGame()
+int StartGame()//начинаем игру
 {
-    int Amount = SetAmountOfTries() , Number = rand() % 99 + 1;
-    for(int i = 0 ; i < Amount ; i++)
+    int Amount = SetAmountOfTries() , Number = rand() % 99 + 1 ; //получаем количество попаыток и определяем число
+    for( int i = 0 ; i < Amount ; i ++ )
     {
-        switch( Game(Number) )
+        switch( Game( Number ) )
         {
             case -1 :
-                return 0;
+                return 0 ;
             default:
-                break;
+                break ;
         }
-        printf("You have %d more tries\n" , Amount - i - 1);
+        printf( "You have %d more tries\n" , Amount - i - 1 ) ;
     }
-    return 0;
+    return 0 ;
 }
 
 
@@ -71,16 +72,16 @@ int main()
 {
     while(TRUE)
     {
-        system("cls");
-        printf("1.New game\n2.Exit\n");
-        char Choose = getch();
-        switch(Choose)
+        system("cls") ;
+        printf( "1.New game\n2.Exit\n" ) ;
+        char Choose = getch() ;
+        switch( Choose )
         {
             case '1' :
-                StartGame();
-                break;
+                StartGame() ;
+                break ;
             case '2' :
-                return 0;
+                return 0 ;
         }
 
 
